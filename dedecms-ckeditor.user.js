d770d5cf17998752616bf9b72c0fac6f7677efc9
chenw <29137819+qfwc258@users.noreply.github.com>
2018-07-27 23:03:38 +0800

    20170728

--- dedecms-ckeditor.user.js
+++ dedecms-ckeditor.user.js
@@ -38,7 +38,7 @@
     }
 
     function createBtn2(){
-        var btn = '<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input name="imageField" type="image" src="images/button_ok.gif" width="60" height="22" class="np" border="0" style="cursor:pointer"></span>';
+        var btn = '<span style="margin-left:10px;"><input name="imageField" type="image" src="images/button_ok.gif" width="60" height="22" class="np" border="0" style="cursor:pointer"></span>';
         var inp = document.getElementById('cw');
         inp.innerHTML+=btn;
     }
