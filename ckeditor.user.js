4a42f6ed4642dd596575351a3f3b39ea23dd01a4
chenw <29137819+qfwc258@users.noreply.github.com>
2018-07-31 16:31:04 +0800

    县市完整

--- ckeditor.user.js
+++ ckeditor.user.js
@@ -7,6 +7,7 @@
 // @include      *hnhbxww.com/adminVIP/login.php*
 // @include      *hnhbxww.com/adminVIP/article_edit.php*
 // @include      *hnhbxww.com/adminVIP/content_list.php*
+// @include      *hnhbxww.com/adminVIP/article_add.php*
 // @license      MIT
 // @run-at       document-end
 // @grant        unsafeWindow
@@ -72,7 +73,7 @@
         //arcrank.removeChild( arcrank.childNodes[1] );
         var newopt=document.createElement('option');
         newopt.setAttribute('value','0');
-        //newopt.text='开放';
+        newopt.innerText='开放浏览';
         arcrank.replaceChild(newopt,arcrank.childNodes[1] );
         //iframe
         var iframe = document.getElementsByTagName('iframe')[0];
@@ -110,7 +111,7 @@
            document.getElementById('typeid2').setAttribute('value','13');
             return;
         };
-        if(zlmtitle.match(/娄星区|冷水江|涟源|新化|双峰/)!=null){
+        if(zlmtitle.match(/娄底|娄星区|冷水江|涟源|新化|双峰/)!=null){
            document.getElementById('typeid2').setAttribute('value','23');
             return;
         };
@@ -122,35 +123,35 @@
            document.getElementById('typeid2').setAttribute('value','15');
             return;
         };
-        if(zlmtitle.match(/赫山区|资阳区|沅江|桃江|南县|安化/)!=null){
+        if(zlmtitle.match(/益阳|赫山区|资阳区|沅江|桃江|南县|安化/)!=null){
            document.getElementById('typeid2').setAttribute('value','77');
             return;
         };
-        if(zlmtitle.match(/永定区|武陵源区|慈利|桑植/)!=null){
+        if(zlmtitle.match(/张家界|永定区|武陵源区|慈利|桑植/)!=null){
            document.getElementById('typeid2').setAttribute('value','19');
             return;
         };
-         if(zlmtitle.match(/武陵区|鼎城区|津市|澧县|临澧|桃源|汉寿|安乡|石门/)!=null){
+         if(zlmtitle.match(/常德|武陵区|鼎城区|津市|澧县|临澧|桃源|汉寿|安乡|石门/)!=null){
            document.getElementById('typeid2').setAttribute('value','17');
             return;
         };
-        if(zlmtitle.match(/北湖区|苏仙区|资兴|宜章|汝城|安仁|嘉禾|临武|桂东|永兴|桂阳/)!=null){
+        if(zlmtitle.match(/郴州|北湖区|苏仙区|资兴|宜章|汝城|安仁|嘉禾|临武|桂东|永兴|桂阳/)!=null){
            document.getElementById('typeid2').setAttribute('value','21');
             return;
         };
-        if(zlmtitle.match(/岳阳楼区|君山区|云溪区|临湘|汨罗|岳阳|湘阴|平江|华容/)!=null){
+        if(zlmtitle.match(/岳阳|岳阳楼区|君山区|云溪区|临湘|汨罗|岳阳|湘阴|平江|华容/)!=null){
            document.getElementById('typeid2').setAttribute('value','16');
             return;
         };
-        if(zlmtitle.match(/双清区|大祥区|北塔区|武冈|邵东|洞口|新邵|绥宁|新宁|邵阳|隆回|城步/)!=null){
+        if(zlmtitle.match(/邵阳|双清区|大祥区|北塔区|武冈|邵东|洞口|新邵|绥宁|新宁|邵阳|隆回|城步/)!=null){
            document.getElementById('typeid2').setAttribute('value','20');
             return;
         };
-        if(zlmtitle.match(/冷水滩区|芝山区|祁阳|蓝山|宁远|新田|东安|江永|道县|双牌|江华/)!=null){
+        if(zlmtitle.match(/永州|冷水滩区|芝山区|祁阳|蓝山|宁远|新田|东安|江永|道县|双牌|江华/)!=null){
            document.getElementById('typeid2').setAttribute('value','22');
             return;
         };
-        if(zlmtitle.match(/鹤城区|洪江|会同|沅陵|辰溪|溆浦|中方|新晃|芷江|通道|靖州|麻阳/)!=null){
+        if(zlmtitle.match(/怀化|鹤城区|洪江|会同|沅陵|辰溪|溆浦|中方|新晃|芷江|通道|靖州|麻阳/)!=null){
            document.getElementById('typeid2').setAttribute('value','18');
             return;
         };
@@ -176,5 +177,13 @@
   /**if (isURL("http://hnhbxww.com/adminVIP/content_list.php?arcrank=-1")) {
       document.getElementsByTagName("a")[0].click();
   }**/
+
+    if (isURL("http://hnhbxww.com/adminVIP/article_add.php")) {
+        //var tr=document.getElementById('typeid')
+      //var tr = document.getElementsByClassName('cke_contents');
+    //var tr = document.getElementsByClassName('cke_show_borders');
+    //alert(tr);
+        //return;
+        }
 
 })();
